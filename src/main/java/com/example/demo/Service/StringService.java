08016d189732e659e01306de1b1d4367795cb01f
Lee <victor.k.lee@ontario.ca>
2020-09-28 11:20:58 -0400

    StringService: use log4j2 for LOG (slf4j)

--- src/main/java/com/example/demo/Service/StringService.java
+++ src/main/java/com/example/demo/Service/StringService.java
@@ -1,9 +1,11 @@
 package com.example.demo.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StringService {
-
+    private static final Logger LOG = LoggerFactory.getLogger(StringService.class);
 
 }

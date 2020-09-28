31b7511147e5c4bf08521baa2468b0f803ad1729
Lee <victor.k.lee@ontario.ca>
2020-09-28 11:25:46 -0400

    StringServiceTest as Spring Component test

--- src/test/java/com/example/demo/Service/StringServiceTest.java
+++ src/test/java/com/example/demo/Service/StringServiceTest.java
@@ -1,6 +1,19 @@
 package com.example.demo.Service;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
 public class StringServiceTest {
+
+    /**
+     *  This is a test class that use Spring Component (StringService), 
+     *  hence we need to bring Test class as part of the Spring by annotating
+     *  @SpringBootTest
+     *  @RunWith(SpringJUnit4ClassRunner.class)
+     */
 
 
 }

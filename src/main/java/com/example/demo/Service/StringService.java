73439659e2f21ade7ec3567eb62efbea1e3aa00f
Lee <victor.k.lee@ontario.ca>
2020-09-28 11:22:21 -0400

    StringService: added replaceString

--- src/main/java/com/example/demo/Service/StringService.java
+++ src/main/java/com/example/demo/Service/StringService.java
@@ -1,5 +1,8 @@
 package com.example.demo.Service;
 
+import com.example.demo.Common.CommonStringUtils;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -7,5 +10,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class StringService {
     private static final Logger LOG = LoggerFactory.getLogger(StringService.class);
+
+    /**
+     * make use of CommonStringUtils (static) and apache StringUtils(maven package)
+     */
+    public String replaceString(String input, String target, String replace) {
+        String result = input;
+        if ((CommonStringUtils.containStrNoCase(input, target)) == true) {
+            result = StringUtils.replace(input, target, replace);
+        }
+        return result;
+    }
 
 }

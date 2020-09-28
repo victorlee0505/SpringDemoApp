30191637dfd826b3d821aaff3ba68f0b625983d2
Lee <victor.k.lee@ontario.ca>
2020-09-28 12:39:35 -0400

    ApiController: second GET api return JSON

--- src/main/java/com/example/demo/Controller/ApiController.java
+++ src/main/java/com/example/demo/Controller/ApiController.java
@@ -1,8 +1,13 @@
 package com.example.demo.Controller;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,5 +45,41 @@ public class ApiController {
 
         return result;
 
+    }
+
+    /**
+     * Default greeting as JSON
+     * http://localhost:8080/api/greeting/get
+     * @return
+     */
+    @GetMapping(value = { "/get" }, produces = "application/json")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "successful operation"),
+            @ApiResponse(responseCode = "204", content = @Content(mediaType = "text/plain"), description = "204"),
+            @ApiResponse(responseCode = "500", content = @Content(mediaType = "text/plain"), description = "500 Please try again. If error persist, contact administrator") })
+    @SuppressWarnings("nls")
+    public ResponseEntity<Map<String, String>> getGreeting2() {
+        LOG.info("/api/greeting/get Started");
+        ResponseEntity<?> entRes = null;
+        Map<String, String> response = new HashMap<String, String>();
+        response.put("Service", "/api/greeting/get");
+
+        try {
+            response.put("Status", "Success");
+            response.put("StatusCode", "200");
+            response.put("Message", "Hello World from Demo");
+        }
+        catch (Exception e) {
+            // LOG.error("Exception encountered, return 500", e);
+            response.put("Status", "Failed");
+            response.put("StatusCode", "500");
+            response.put("Message", "Failed.");
+        }
+
+        entRes = ResponseEntity.status(HttpStatus.OK).body(Collections.unmodifiableMap(response));
+        @SuppressWarnings("unchecked")
+        final ResponseEntity<Map<String, String>> resEnt = (ResponseEntity<Map<String, String>>) entRes;
+
+        LOG.info("/api/greeting/get Finished");
+        return resEnt;
     }
 }

ad7ae2c5df9e1f6aebf60790fd068759a9b4be7b
Lee <victor.k.lee@ontario.ca>
2020-09-28 11:43:15 -0400

    ApiController: RequestMapping

--- src/main/java/com/example/demo/Controller/ApiController.java
+++ src/main/java/com/example/demo/Controller/ApiController.java
@@ -1,8 +1,14 @@
 package com.example.demo.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Request Mapping to localhost:8080/api/greeting
+ */
 @RestController
+@RequestMapping("/api/greeting")
 public class ApiController {
     
+
 }

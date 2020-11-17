f412a8f75dd84fdec3726e5ebef40a60f9b8d96a
Lee <victor.k.lee@ontario.ca>
2020-11-17 09:32:17 -0500

    Entity use lombok

--- src/main/java/com/example/demo/Database/Entity/Country.java
+++ src/main/java/com/example/demo/Database/Entity/Country.java
@@ -2,7 +2,17 @@ package com.example.demo.Database.Entity;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Country {
     String name;
     String nativeName;
--- src/main/java/com/example/demo/Database/Entity/Top.java
+++ src/main/java/com/example/demo/Database/Entity/Top.java
@@ -2,6 +2,17 @@ package com.example.demo.Database.Entity;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Top {
     int page;
     int per_page;

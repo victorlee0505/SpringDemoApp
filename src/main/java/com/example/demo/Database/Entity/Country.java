cf9b15b77f8b55de457563769a48752af6ea4fa1
Lee <victor.k.lee@ontario.ca>
2020-11-17 09:31:27 -0500

    Entity: added Top & Country as Json parsing VO

---
+++ src/main/java/com/example/demo/Database/Entity/Country.java
@@ -0,0 +1,27 @@
+package com.example.demo.Database.Entity;
+
+import java.util.List;
+
+
+public class Country {
+    String name;
+    String nativeName;
+    List<String> topLevelDomain;
+    String alpha2Code;
+    List<String> currencies;
+    List<String> callingCodes;
+    String capital;
+    List<String> altSpellings;
+    String relevance;
+    String region;
+    String subregion;
+    List<String> language;
+    List<String> languages;
+    int population;
+    List<Integer> latlng;
+    String demonym;
+    List<String> borders;
+    int area;
+    double gini;
+    List<String> timezones;
+}
---
+++ src/main/java/com/example/demo/Database/Entity/Top.java
@@ -0,0 +1,11 @@
+package com.example.demo.Database.Entity;
+
+import java.util.List;
+
+public class Top {
+    int page;
+    int per_page;
+    int total;
+    int total_pages;
+    List<Country> data;
+}

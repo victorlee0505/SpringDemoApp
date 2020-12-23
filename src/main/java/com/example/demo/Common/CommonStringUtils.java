236bb5362ceecc74350d4ef7f8123f1625d40b77
Lee <victor.k.lee@ontario.ca>
2020-12-23 13:18:51 -0500

    CommonStringUtils: added 2 Map to String/Json method

--- src/main/java/com/example/demo/Common/CommonStringUtils.java
+++ src/main/java/com/example/demo/Common/CommonStringUtils.java
@@ -1,23 +1,38 @@
 package com.example.demo.Common;
 
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.apache.commons.lang3.StringUtils;
 
 public class CommonStringUtils {
 
-    public static boolean containChar(String input, char target) { 
+    public static boolean containChar(String input, char target) {
         for (int i = 0; i < input.length(); i++) {
             if (input.charAt(i) == target) {
                 return true;
-            } 
+            }
         }
         return false;
-    } 
+    }
 
     public static boolean containStrNoCase(String input, String target) {
         String x = "Hello world";
         boolean result = false;
         result = StringUtils.containsIgnoreCase(input, target);
         return result;
+    }
+
+    public static String convertToString(Map<String, String> map) {
+        return StringUtils.join(map);
+    }
+
+    public static String convertToJson(Map<String, String> map) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        
+        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
     }
     
 }
--- src/test/java/com/example/demo/Common/CommonStringUtilsTest.java
+++ src/test/java/com/example/demo/Common/CommonStringUtilsTest.java
@@ -2,24 +2,59 @@ package com.example.demo.Common;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import org.junit.Test;
 
 public class CommonStringUtilsTest {
-    
-    @Test 
+
+    @Test
     public void testContainChar() {
         String name = "Tiffany";
         char target = 'i';
         boolean actual = CommonStringUtils.containChar(name, target);
         assertTrue(actual);
     }
-    
-    @Test 
+
+    @Test
     public void testContainStrNoCase() {
         String name = "Tiffany";
         String target = "I";
         boolean actual = CommonStringUtils.containStrNoCase(name, target);
         assertTrue(actual);
     }
-    
+
+    @Test
+    public void testMaptoString() {
+        Map<String, String> params = new HashMap<String, String>();
+
+        params.put("application", "vss");
+        params.put("region", "M");
+        params.put("operator", "id");
+
+        String text = CommonStringUtils.convertToString(params);
+        System.out.println(text);
+    }
+
+    @Test
+    public void testMaptoJson() {
+        Map<String, String> params = new HashMap<String, String>();
+
+        params.put("application", "vss");
+        params.put("region", "M");
+        params.put("operator", "id");
+
+        String text;
+        try {
+            text = CommonStringUtils.convertToJson(params);
+            System.out.println(text);
+        } catch (JsonProcessingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+    }
 }

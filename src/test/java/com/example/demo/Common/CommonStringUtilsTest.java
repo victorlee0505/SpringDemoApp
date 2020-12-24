c38797e388fb72bb2a2b6dec0404ad6e763ae338
Lee <victor.k.lee@ontario.ca>
2020-12-24 11:38:40 -0500

    Test constructing a JSON message

--- src/test/java/com/example/demo/Common/CommonStringUtilsTest.java
+++ src/test/java/com/example/demo/Common/CommonStringUtilsTest.java
@@ -7,6 +7,8 @@ import java.util.Map;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 
 public class CommonStringUtilsTest {
@@ -42,19 +44,52 @@ public class CommonStringUtilsTest {
     @Test
     public void testMaptoJson() {
         Map<String, String> params = new HashMap<String, String>();
-
         params.put("application", "vss");
         params.put("region", "M");
         params.put("operator", "id");
 
-        String text;
+        Map<String, String> jsonMap = new HashMap<String, String>();
+
+        // jsonMap.put("body", "test1");
+
         try {
-            text = CommonStringUtils.convertToJson(params);
-            System.out.println(text);
+            String jsonParam = CommonStringUtils.convertToJson(params);
+            System.out.println(jsonParam);
+            jsonMap.put("parameterMap", jsonParam);
+            System.out.println(CommonStringUtils.convertToJson(jsonMap));
         } catch (JsonProcessingException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        
+
+    }
+
+    @Test
+    public void testJSONArray() {
+        Map<String, String> parameters = new HashMap<String, String>();
+        parameters.put("application", "vss");
+        parameters.put("region", "M");
+        parameters.put("operator", "id");
+
+        try {
+            JSONObject bodyJson = new JSONObject();
+            bodyJson.put("body", "test1");
+
+
+            JSONObject parameterJson = new JSONObject();
+            for (Map.Entry<String, String> param : parameters.entrySet()) {
+                parameterJson.put(param.getKey(), param.getValue());
+            }
+
+            bodyJson.put("parameterMap", parameterJson);
+
+            String json = bodyJson.toString();
+
+            System.out.println(json);
+
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
